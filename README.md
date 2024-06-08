db4fbe1c081c90cffca56d4a3ddce4a4f32451c6
0zd0 <67220210+0zd0@users.noreply.github.com>
2024-06-08 21:50:27 +0300

    fix/dev imports

--- README.md
+++ README.md
@@ -7,3 +7,30 @@ Install the plugin
 ```bash
 bun install rollup-plugin-woocommerce-dependency-extraction --save-dev
 ```
+
+## Vite
+
+```ts
+export default defineConfig(({ mode }) => {
+    return {
+        plugins: [
+            react(),
+            wcDependencyExtraction(mode === 'development'),
+        ],
+        build: {
+            manifest: true,
+            rollupOptions: {
+                output: {
+                    format: 'iife',
+                },
+            },
+        },
+        esbuild: {
+            minifyIdentifiers: false,
+        },
+        optimizeDeps: {
+            exclude: [...wcOptimizeDepsExclude],
+        },
+    }
+})
+```
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import type { ExternalOption, InputOptions, OutputOptions, Plugin } from 'rollup';
+import type { ExternalOption, InputOptions, OutputOptions, Plugin } from 'rollup'
 
-const ns = '@woocommerce/';
-const nsExclude: string[] = [];
-const wcMatch = new RegExp(`^${ns}(?!(${nsExclude.join('|')})).*$`);
+const ns = '@woocommerce/'
+const nsExclude: string[] = []
+const wcMatch = new RegExp(`^${ns}(?!(${nsExclude.join('|')})).*$`)
 
 const wcExternal: Record<string, string> = {
     // wc-blocks packages
@@ -12,52 +12,73 @@ const wcExternal: Record<string, string> = {
     '@woocommerce/blocks-registry': 'wc.wcBlocksRegistry',
     '@woocommerce/price-format': 'wc.priceFormat',
     '@woocommerce/settings': 'wc.wcSettings',
-};
+}
 
-export default function wcDependencyExtraction(): Plugin {
+export const wcOptimizeDepsExclude = Object.keys(wcExternal)
+
+export default function wcDependencyExtraction(isDev: boolean): Plugin {
     return {
         name: 'wc-dependency-extraction',
         options: (options: InputOptions) => {
             if (!Array.isArray(options.external)) {
-                options.external = [options.external].filter(Boolean) as ExternalOption;
+                options.external = [options.external].filter(Boolean) as ExternalOption
             }
 
             if (Array.isArray(options.external)) {
-                options.external = options.external.concat(Object.keys(wcExternal));
-                options.external.push(wcMatch);
+                options.external = options.external.concat(Object.keys(wcExternal))
+                options.external.push(wcMatch)
             }
 
-            return options;
+            return options
         },
         outputOptions: (outputOptions: OutputOptions) => {
-            const configGlobals = outputOptions.globals;
+            const configGlobals = outputOptions.globals
 
             outputOptions.globals = (id: string) => {
                 if (typeof configGlobals === 'object' && id in configGlobals && configGlobals[id]) {
-                    return configGlobals[id];
+                    return configGlobals[id]
                 }
 
                 if (typeof configGlobals === 'function') {
-                    const configGlobalId = configGlobals(id);
+                    const configGlobalId = configGlobals(id)
 
                     if (configGlobalId && configGlobalId !== id) {
-                        return configGlobalId;
+                        return configGlobalId
                     }
                 }
 
                 if (id in wcExternal && wcExternal[id]) {
-                    return wcExternal[id];
+                    return wcExternal[id]
                 }
 
                 if (wcMatch.test(id)) {
                     return id
                         .replace(new RegExp(`^${ns}`), 'wc.')
                         .replace(/\//g, '.')
-                        .replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+                        .replace(/-([a-z])/g, (_, letter) => letter.toUpperCase())
                 }
 
-                return '';
-            };
+                return ''
+            }
         },
-    };
+        transform(code: string, id: string) {
+            if (isDev && id.endsWith('.ts') || id.endsWith('.tsx')) {
+                Object.keys(wcExternal).forEach((key) => {
+                    const value = wcExternal[key]
+                    const regex = new RegExp(
+                        `import\\s*{([^}]*)}\\s*from\\s*['"]${key}['"]`,
+                        'g',
+                    )
+                    code = code.replace(
+                        regex,
+                        (_, imports) => `const {${imports.trim()}} = ${value};`,
+                    )
+                })
+                return {
+                    code,
+                    map: null,
+                }
+            }
+        },
+    }
 }
